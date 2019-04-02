e6f9e699b24d1b2276982dd4ab65107f4f0d8a10
Nursultan <nursmaster@gmail.com>
2019-04-02 13:17:21 +0300

    I separated the header from the navbar for convenience

---
+++ application/views/templates/navbarDashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome --> 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="<?php echo base_url('bst/css/bootstrap.min.css')?>" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href=" <?php echo base_url('bst/css/mdb.min.css')?>" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="<?php echo base_url('bst/css/style.css')?>" rel="stylesheet">
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+    <div class="container">
+      <a href="#" class="navbar-brand">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+        aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="basicExampleNav">
+        <ul class="navbar-nav mr-auto smooth-scroll">
+          <li class="nav-item">
+            <a href="#intro" class="nav-link waves-effect wawes-light">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#best-features" class="nav-link waves-effect wawes-light">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="#examples" class="nav-link waves-effect wawes-light">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a href="#gallery" class="nav-link waves-effect wawes-light"></a>
+          </li>
+         
+        </ul>
+        <div class="">
+        
+        </div>
+      </div>
+    </div>
+  </nav>
