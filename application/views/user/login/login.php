dfbb31265fbfa6c4452e1f4f196299fc5999998c
Long Vo <gaming2000hcm@gmail.com>
2019-04-02 11:51:56 +0300

    2nd change to login register

--- application/views/user/login/login.php
+++ application/views/user/login/login.php
@@ -1,7 +1,32 @@
-<!-- Material form login -->
-<div class="card">
+<!DOCTYPE html>
+<html lang="en">
 
-  <h5 class="card-header info-color white-text text-center py-4">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="<?php echo base_url('bst/css/bootstrap.min.css')?>" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href=" <?php echo base_url('bst/css/mdb.min.css')?>" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="<?php echo base_url('bst/css/style.css')?>" rel="stylesheet">
+  <style>
+    body {
+      background: url(https://images.unsplash.com/photo-1525642351754-db88d9142dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1764&q=80) no-repeat center fixed;
+      background-size: 100% 100%;
+    }
+  </style>
+</head>
+
+<body>
+<!-- Material form login -->
+<div class="card centered mt-4 rounded-30 " style="width:40%; margin:auto;">
+
+  <h5 class="card-header white-text text-center py-4" style="background-color:black;">
     <strong>Sign in</strong>
   </h5>
 
@@ -40,14 +65,14 @@
       </div>
 
       <!-- Sign in button -->
-      <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+      <button class="btn btn-outline-form btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
 
       <!-- Register -->
       <p>Not a member?
-        <a href="<?php echo site_url('user/register');?>" style="color:#2d72e2;">Register</a>
+        <a href="" style="color:#2d72e2;">Register</a>
       </p>
 
-      <!-- Social login -->
+      <!-- Social login
       <p>or sign in with:</p>
       <a type="button" class="btn-floating btn-fb btn-sm">
         <i class="fab fa-facebook-f"></i>
@@ -61,7 +86,7 @@
       <a type="button" class="btn-floating btn-git btn-sm">
         <i class="fab fa-github"></i>
       </a>
-
+      -->
     </form>
     <!-- Form -->
 
--- application/views/user/login/register.php
+++ application/views/user/login/register.php
@@ -1,7 +1,31 @@
-<!-- Material form register -->
-<div class="card">
+<!DOCTYPE html>
+<html lang="en">
 
-    <h5 class="card-header info-color white-text text-center py-4">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="<?php echo base_url('bst/css/bootstrap.min.css')?>" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href=" <?php echo base_url('bst/css/mdb.min.css')?>" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="<?php echo base_url('bst/css/style.css')?>" rel="stylesheet">
+  <style>
+  body {
+    background: url(https://images.unsplash.com/photo-1525642351754-db88d9142dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1764&q=80) no-repeat center fixed;
+    background-size: 100% 100%;
+  }
+  </style>
+</head>
+
+<body>
+<!-- Material form register -->
+<div class="card centered mt-4 rounded-30" style="width:40%; margin:auto;" >
+    <h5 class="card-header white-text text-center py-4" style="background-color:black;" >
         <strong>Sign up</strong>
     </h5>
 
@@ -22,7 +46,7 @@
                 <div class="col">
                     <!-- Last name -->
                     <div class="md-form">
-                        <input type="text" id="materialRegisterFormLastName" class="form-control">
+                        <input type="email" id="materialRegisterFormLastName" class="form-control">
                         <label for="materialRegisterFormLastName">Last name</label>
                     </div>
                 </div>
@@ -58,9 +82,10 @@
             -->
 
             <!-- Sign up button -->
-            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign up</button>
+            <button class="btn btn-outline-form btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign up</button>
 
             <!-- Social register -->
+            <!--
             <p>or sign up with:</p>
 
             <a type="button" class="btn-floating btn-fb btn-sm">
@@ -75,6 +100,7 @@
             <a type="button" class="btn-floating btn-git btn-sm">
                 <i class="fab fa-github"></i>
             </a>
+            -->
 
             <hr>
 
