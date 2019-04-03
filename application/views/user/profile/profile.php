9bc483b8f89344065dc84cc9b0e08031e66cd716
Dmitrii <kislenko1910@mail.ru>
2019-04-03 13:28:26 +0300

    Added date

--- application/views/user/profile/profile.php
+++ application/views/user/profile/profile.php
@@ -6,18 +6,17 @@ Hello  -->
             <div class="col-md-6">
                 <img src="https://im0-tub-ru.yandex.net/i?id=f863e4b6e729c52c6d2c53e8428ff71f&n=13&exp=1" alt="avatar">
             </div>
-            <div class="col-md-6">
-                <h1>
+            <div class="col-md-6 text-left">
+                <h2>
                     Username: 
                     <?php echo $_SESSION["username"] ?> <br>
                     Date of registration:
-                    <?php echo ""?>
+                    <?php echo $time?>
 
-                </h1>
+                </h2>
             </div>
         </div>
     </div>
     <hr>
+  
 <?php include("footerProfile.php") ?>
-
-
