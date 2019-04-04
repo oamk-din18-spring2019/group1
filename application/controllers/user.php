3d71bc4152966e57efc9403f099afda7c56f31c2
Dmitrii <kislenko1910@mail.ru>
2019-04-04 09:53:09 +0300

    FInished profile

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -5,6 +5,7 @@ class User extends CI_Controller
     {
         parent::__construct();
         $this->load->model('User_model');
+        $this->load->model('Search_model');
     }
 
     public function index()
@@ -90,10 +91,8 @@ class User extends CI_Controller
         else{
             echo "You are not registred";
         }
-      
+
     }
-
-
     public function getConvos()
     { }
 
@@ -101,5 +100,12 @@ class User extends CI_Controller
         $data['username'] = $username;
         $this->load->view('user/chat/chat_screen', $data);
     }
+
+    public function search()
+    {
+        $this->load->view('user/search/search');
+        $data['cari'] = $this->Search_model->cariTest();
+        $this->load->view('user/search/searchresult', $data);
+    }
+
 }
-    
