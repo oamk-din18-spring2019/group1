6d93177a3061f8e42c3ab665930c1375b6466801
Dmitrii <kislenko1910@mail.ru>
2019-04-05 11:59:02 +0300

    Fixed the access without registration

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller
 {
     public function __construct()
@@ -6,74 +7,23 @@ class User extends CI_Controller
         parent::__construct();
         $this->load->model('User_model');
         $this->load->model('Search_model');
+        if (!empty($_SESSION['logged_in']) && $_SESSION['logged_in']==true){
+      } else{
+        //   $this->load->view("user/login/login"); 
+        // redirect("main_page");
+        header('location:access_denied');
+        }
     }
-
+function access_denied(){
+    redirect("LoginRegistration/login");
+}
     public function index()
     {
         $data['activeFriends'] = $this->User_model->activeFriends();
         $data['page'] = 'user/dashboard';
         $this->load->view('templates/content', $data);
     }
-    public function login()
-    {
-        $this->load->view('user/login/login');
-    }
-
-    public function register()
-    {
-        $this->load->view('user/login/register');
-    }
-
-    function add_user()
-    {
-        //Registration function
-        // Checks the passwords to be equal
-        if ($this->input->post('pw1') == $this->input->post('pw2')) {
-            //hash the password and send the information to the database
-                $hashedPassword = password_hash($this->input->post('pw1'), PASSWORD_DEFAULT);
-                $insert_data = array(
-                    "username" => $this->input->post('un'),
-                    "email" => $this->input->post('em'),
-                    "passwd" => $hashedPassword
-                );
-                $result = $this->User_model->add_user($insert_data);
-                if ($result == 1) {
-                    $data['message'] = "Registration passed succesful";
-                    $this->load->view('user/login/login', $data);
-                } else {
-                //checks if the username is unique, but for this we have to change row "username" in db to be unique
-                    $data['message'] = "This username is used already";
-                    $this->load->view('user/login/register', $data);
-                }
-            } else {
-            $data['message'] = "You entered the different passwords";
-            $this->load->view('user/login/register', $data);
-        }
-    }
-
-    function log_in_procedure()
-    {
-    //function for entering the system
-        $givenUsername = $this->input->post('username');
-        $givenPassword = $this->input->post('password');
-        $db_password = $this->User_model->getPassword($givenUsername);
-        //verify the password
-        if (password_verify($givenPassword, $db_password)) {
-            $_SESSION['logged_in'] = true;
-            $_SESSION['username'] = $givenUsername;
-            $_SESSION['image']=$this->User_model->getPictureName($_SESSION['username']);
-            $data['message'] = "Succesful";
-            // $this->load->view('user/profile');
-            header('location:profile');
-        } else {
-            $_SESSION['logged_in'] = false;
-            $data['messagePassword']="Wrong password or username";
-            $this->load->view('user/login/login', $data);
-        }
-
-        // $data['page']='users/';
-        // $this->load->view('templates/content',$data);
-    }
+   
 
     public function profile()
     {
@@ -91,7 +41,6 @@ class User extends CI_Controller
             $this->load->view('user/profile/profile');
         }
         else{
-            echo "You are not registred";
         }
 
 
