b7d8a2247a8694a352200cec8d3239a791cbb2ea
Dmitrii <kislenko1910@mail.ru>
2019-04-11 13:43:56 +0300

    Small change of layout

--- application/views/user/argument/answer.php
+++ application/views/user/argument/answer.php
@@ -1,16 +1,21 @@
 <?php
 echo "You choosed " . $category . " <br>";
-echo "Your question is " . $question;
+// echo "Your question is " . print_r($question);
 ?>
-<h2> Do you agree?</h2>
-<form class="text-left" method="POST" action="<?php echo site_url('user/getAnswer')?>">
-    <input type="submit" class="btn btn-success" id="BoxSelect" value="Submit">
-    <div class="custom-control custom-radio">
-        <input type="radio" class="custom-control-input" id="defaultChecked" value="1" name="defaultExampleRadios" checked>
-        <label class="custom-control-label" for="defaultChecked">Yes,I agree</label>
+<?php 
+for ($i=0;$i<count($question);$i++){
+echo $question[$i]['content'];
+echo '<br>';
+echo "<form class='text-left' method='POST' action=".site_url('user/getAnswer').">
+<input type='submit' class='btn btn-success' id='BoxSelect' value='Submit'>
+    <div class='custom-control custom-radio'>
+        <input type='radio' class='custom-control-input' id='defaultChecked' value=1' name='defaultExampleRadios' checked>
+        <label class='custom-control-label' for='defaultChecked'>Yes,I agree</label>
     </div>
-    <div class="custom-control custom-radio">
-        <input type="radio" class="custom-control-input" id="defaultUnchecked" value="0" name="defaultExampleRadios">
-        <label class="custom-control-label" for="defaultUnchecked">No, I disagree</label>
+    <div class='custom-control custom-radio'>
+        <input type='radio' class='custom-control-input' id='defaultUnchecked' value='0' name='defaultExampleRadios'>
+        <label class='custom-control-label' for='defaultUnchecked'>No, I disagree</label>
     </div>  
-</form>
+</form>";
+}
+?>
