6f94d3c609b331e604e91bfca0fabf0ae923cb30
Dmitrii <kislenko1910@mail.ru>
2019-04-12 13:06:46 +0300

    You can choose your opinions

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -161,9 +161,15 @@ class User_model extends CI_Model{
         //
         // This system returns random question from motions 
         // $numberOfRows=$this->db->get()->row('COUNT(*)');  
-         return $this->db->query("SELECT idMotion,content from motions where category='$category' group by content;")->result_array(); 
+        
+         return $this->db->query("SELECT motions.idMotion,category,content,if(agree=0 or agree=1,agree,null) as agree from motions  left join  opinions on opinions.idMotion=motions.idMotion where (agree is null and(category='$category')) ; 
+         ")->result_array(); 
         // return( $arrayOfMotions[rand(0,$numberOfRows-1)]['content']);
         //
+    }
+    public function showAnsweredMotions($category){
+        return $this->db->query("SELECT motions.idMotion,category,content,if(agree=0 or agree=1,agree,null) as agree from motions  left join  opinions on opinions.idMotion=motions.idMotion where (agree is not null and(category='$category')) order by agree ;
+        ")->result_array(); 
     }
     public function addOpinion($idMotion,$idUser,$opinion){
         $this->db->query("INSERT INTO `opinions` (`id`, `idMotion`, `idUser`, `Agree`) VALUES (NULL, '$idMotion', '$idUser', '$opinion')");
--- application/views/user/argument/answer.php
+++ application/views/user/argument/answer.php
@@ -1,21 +1,39 @@
 <?php
-echo "You choosed " . $category . " <br>";
+echo "You chose " . $category . " <br>";
 // echo "Your question is " . print_r($question);
 ?>
 <?php 
-for ($i=0;$i<count($question);$i++){
-echo $question[$i]['content'];
-echo '<br>';
-echo "<form class='text-left' method='POST' action=".site_url('Motion/getAnswer/').$question[$i]['idMotion'].">
-<input type='submit' class='btn btn-success' id='BoxSelect' value='Submit'>
-    <div class='custom-control custom-radio'>
-        <input type='radio' class='custom-control-input' id='defaultChecked' value=1 name='defaultExampleRadios' checked>
-        <label class='custom-control-label' for='defaultChecked'>Yes,I agree</label>
-    </div>
-    <div class='custom-control custom-radio'>
-        <input type='radio' class='custom-control-input' id='defaultUnchecked' value='0' name='defaultExampleRadios'>
-        <label class='custom-control-label' for='defaultUnchecked'>No, I disagree</label>
-    </div>  
-</form>";
+if( $question)
+{
+    for ($i=0;$i<count($question);$i++){
+        echo $question[$i]['content'];
+        echo '<br>';
+        echo "<form class='text-left' method='POST' action=".site_url('Motion/getAnswer/').$question[$i]['idMotion'].">
+        <input type='submit' class='btn btn-success' id='BoxSelect' value='Submit'>
+            <div class='custom-control custom-radio'>
+                <input type='radio' class='custom-control-input' id='defaultChecked' value=1 name='defaultExampleRadios' checked>
+                <label class='custom-control-label' for='defaultChecked'>Yes,I agree</label>
+            </div>
+            <div class='custom-control custom-radio'>
+                <input type='radio' class='custom-control-input' id='defaultUnchecked' value='0' name='defaultExampleRadios'>
+                <label class='custom-control-label' for='defaultUnchecked'>No, I disagree</label>
+            </div>  
+        </form>";
+        }
+} else {
+    echo " <h2>You answered all the questions</h2>";
 }
+if ($answeredMotions){
+    echo  " <h2>Your answers:</h2> <br> ";
+    for ($i=0;$i<count($answeredMotions);$i++){
+        echo $answeredMotions[$i]['content'].': ';
+        if ($answeredMotions[$i]['agree']=1){
+            echo 'I agree <br>';
+        } else {
+            echo ' I disagree <br>';
+        }
+    };
+}
+
+    
 ?>
