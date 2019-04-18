ae22558245bbecad5e2806ac6137e8e6ff1d8b14
Long <longlong2000hcm@gmail.com>
2019-04-18 13:22:25 +0300

    Added respondsive to UI chat

--- application/views/user/chat/messenger.php
+++ application/views/user/chat/messenger.php
@@ -4,8 +4,6 @@
     <iframe class="col-lg-2 border border-dark rounded-left" style="box-sizing:border-box;" src="<?php echo site_url('user/allConversations').'/'.$_SESSION['username'] ?>"></iframe>
     <!-- Display chat screen on the same view with chat list -->
     <iframe class="col-10 border border-dark rounded-right d-none d-lg-block" style="box-sizing:border-box;" src="<?php echo site_url('user/chat/'.$username) ?>" name=chatScreen></iframe>
-    <!-- Display chat screen on the different view with chat list -->
-    <iframe class="col-10 border border-dark rounded-right d-none d-lg-block" style="box-sizing:border-box;" src="<?php echo site_url('user/chat/'.$username) ?>" name=chatScreen></iframe>
   </div>
   </div>
 </div>
