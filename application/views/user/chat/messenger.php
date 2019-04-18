70a47dea6f1de1861d770843497eccc142e31064
Long <longlong2000hcm@gmail.com>
2019-04-18 13:33:17 +0300

    improved UI responsive

--- application/views/user/chat/messenger.php
+++ application/views/user/chat/messenger.php
@@ -1,4 +1,4 @@
-<div class="container mt-4">
+<div class="container-fluid mt-4">
   <div class="row no-gutters justify-content-center" style="height:80vh;">
     <!-- Display chat list -->
     <iframe class="col-lg-2 border border-dark rounded-left" style="box-sizing:border-box;" src="<?php echo site_url('user/allConversations').'/'.$_SESSION['username'] ?>"></iframe>
