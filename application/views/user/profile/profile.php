2f9907d98f19c40a2149943d91c589e8c2545a2b
Long <longlong2000hcm@gmail.com>
2019-04-24 13:24:18 +0300

    hide rating yourself and add achievements to profile page

--- application/views/user/profile/profile.php
+++ application/views/user/profile/profile.php
@@ -4,17 +4,17 @@ Hello  -->
 <div class="container ">
     <div class="row">
     <div class="col-md-1"></div>
-        <div class="col-md-4 col-sm-4">
+        <div class="col-md-4 col-sm-4 text-center">
             <img class="img-fluid rounded z-depth-1 my-3" src="<?php if(isset($_SESSION['image'])&&($_SESSION['image']!='')){ echo base_url("./images/".$_SESSION['image']) ;} else{echo base_url("./images/empty-avatar.jpg");} ?>" alt="avatar">
         </div>
         <div class="col-md-1"></div>
         <div class="col-md-6 text-left mt-4">
-           
-            
-                <h1><strong><?php echo $_SESSION["username"] ?></strong></h1>  
-               
+
+
+                <h1><strong><?php echo $_SESSION["username"] ?></strong></h1>
+
                <h3>
-                <?php 
+                <?php
                 if($this->User_model->getRating($_SESSION['username'])>=0 and $this->User_model->getRating($_SESSION['username'])<4){
                     echo "<i class='fas fa-user-graduate my-3'></i> Usual user <br>";
                 }
@@ -33,6 +33,100 @@ Hello  -->
                 <i class="fas fa-star my-3"></i>
                 <?php echo $this->User_model->getRating($_SESSION['username']); ?>
             </h3>
+
+            <hr>
+            <h2 class="m-2">
+              <i class="fas fa-trophy"></i>
+              Achivements
+            </h2>
+
+            <?php
+            if ($selectedAchievements[0]=="") {
+              echo '
+              <div class="px-2">
+              You can choose your achievements to show to other people in statistics tab below.
+              </div>
+            ';}
+            ?>
+            <div class="row border border-dark m-2 rounded p-2"
+            <?php
+            if (array_search("time",$selectedAchievements)===false) {echo "hidden";}
+            ?>
+            >
+                <div class="col-2 my-auto text-center pr-0">
+                    <i class="fas fa-stopwatch fa-2x"></i>
+                </div>
+                <div class="col-3 my-auto p-0">
+                    <b><?php if($statistics['time']==1){echo $statistics['time'].' day';} else {echo $statistics['time'].' days';} ?></b>
+                </div>
+                <div class="col-7 my-auto ">
+                    This person has joined
+                </div>
+            </div>
+
+            <div class="row border border-dark m-2 rounded p-2"
+            <?php
+            if (array_search("following",$selectedAchievements)===false) {echo "hidden";}
+            ?>
+            >
+                <div class="col-2 my-auto text-center pr-0">
+                    <i class="fas fa-users fa-2x"></i>
+                </div>
+                <div class="col-3 my-auto p-0">
+                    <b><?php echo ($statistics['following']);?>&nbsp;people</b>
+                </div>
+                <div class="col-7 my-auto ">
+                    Are stalked by this person
+                </div>
+            </div>
+
+            <div class="row border border-dark m-2 rounded p-2"
+            <?php
+            if (array_search("numberOfAnsweredOpinions",$selectedAchievements)===false) {echo "hidden";}
+            ?>
+            >
+                <div class="col-2 my-auto text-center pr-0">
+                    <i class="fas fa-question fa-2x"></i>
+                </div>
+                <div class="col-3 my-auto p-0">
+                    <b><?php echo ($statistics['numberOfAnsweredOpinions']) ?>  &nbsp; answers</b>
+                </div>
+                <div class="col-7 my-auto ">
+                    Were given by this person
+                </div>
+            </div>
+
+            <div class="row border border-dark m-2 rounded p-2"
+            <?php
+            if (array_search("numberOfChosenCategories",$selectedAchievements)===false) {echo "hidden";}
+            ?>
+            >
+                <div class="col-2 my-auto text-center pr-0">
+                    <i class="fas fa-check fa-2x"></i>
+                </div>
+                <div class="col-3 my-auto p-0">
+                    <b><?php echo ($statistics['numberOfChosenCategories']) ?>  &nbsp;categories</b>
+                </div>
+                <div class="col-7 my-auto ">
+                    Were chosen by this person
+                </div>
+            </div>
+
+            <div class="row border border-dark m-2 rounded p-2"
+            <?php
+            if (array_search("rating",$selectedAchievements)===false) {echo "hidden";}
+            ?>
+            >
+                <div class="col-2 my-auto text-center pr-0">
+                    <i class="fas fa-check fa-2x"></i>
+                </div>
+                <div class="col-3 my-auto p-0">
+                    <b><?php echo ($statistics['rating']) ?> &nbsp;points</b>
+                </div>
+                <div class="col-7 my-auto ">
+                    This person got
+                </div>
+            </div>
         </div>
     </div>
 </div>
