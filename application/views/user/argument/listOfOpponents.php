fa6890d645df2e0885c1fa30563a16042db9ae40
Dmitrii <kislenko1910@mail.ru>
2019-04-24 13:20:28 +0300

    Made list of opponents

--- application/views/user/argument/listOfOpponents.php
+++ application/views/user/argument/listOfOpponents.php
@@ -1,45 +1,39 @@
-<div class="container">
-<h2>Your possible opponents:</h2>
-<?php if (isset($opponents[0])){
-        foreach ($opponents as $value){
-          echo ' <div class="card my-4">
+<div class="container mt-2">
+  <h2>Your possible opponents:</h2>
+  <?php if (isset($opponents[0])) {
+    foreach ($opponents as $value) {
+       echo '<div class="container">';
+      echo '<a style="" href="' . site_url('user/others_profile?username=') . $value['username'] . '">';
+     
+      echo ' <div class="card col-md-10 my-3">
           <div class="card-body  px-4 mx-3">';
-          echo '<div class="">
+      echo '<div class=" text-center">
           <div class="container-fluid "> 
           <div class="row">';
-          
-          echo ' <img  style="width:70px; height:70px;" class="img-fluid rounded mb-2 " src="';
-          if(!is_null($value['picture'])&&$value['picture']!=''){
-            echo base_url("./images/").$value['picture'];
-          }
-          else {
-            echo base_url("./images/empty-avatar.jpg");
-          }
-          echo '">';
-          echo   '<h1 class="pl-3">'.$value['username'].'</h1> ';
-          echo '</div>
-          </div> ';
-
-        
-              
-           echo   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita mollitia quam soluta culpa, doloribus nesciunt. Ipsa corrupti voluptate ad, ut earum, voluptatem atque, impedit iure vitae sunt molestiae veritatis quod.
-              <br>
-              <a href="#"><button class="btn btn-info"> Go</button></a>
-          </div>
-      </div> </div>';
-          echo '<a style="" href="'.site_url('user/others_profile?username=').$value['username'].'">';
-          echo '<img src="';
-          if(!is_null($value['picture'])&&$value['picture']!=''){
-            echo base_url("./images/").$value['picture'];
-          }
-          else {
-            echo base_url("./images/empty-avatar.jpg");
-          }
-          echo '" class="mr-3 z-depth-0 rounded " alt="avatar_image" style="width:25px; height:25px;">';
-          echo $value['username']."</a>";
-        }
+      echo "<div class='col-md-3'>";
+      echo ' <img  style="" class="img-fluid rounded mb-2 " src="';
+      if (!is_null($value['picture']) && $value['picture'] != '') {
+        echo base_url("./images/") . $value['picture'];
       } else {
-        echo "<h2>Unfortunately there is no opponent for you :(</h2>";
+        echo base_url("./images/empty-avatar.jpg");
       }
- ?>
+      echo '">';
+      echo '</div>';
+      echo '<div class="col-md-9">';
+      echo '<div class="row">';
+      echo '<div class="col-md-12 text-center">';
+      echo   '<h2  class="pl-3">' . $value['username'] . '</h2> ';
+      echo '</div>';
+      echo '<div class="row mx-2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita mollitia quam soluta culpa. </div>
+          </div>
+          </div>
+          </div>
+          </div> ';
+      echo '</div></div>
+      </div> </div> </a>';
+    }
+  } else {
+    echo "<h2>Unfortunately there is no opponent for you :(</h2>";
+  }
+  ?>
 </div>
