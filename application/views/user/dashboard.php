15f77209d4ba45cac7051c2fe47890f507417131
Nursultan <nursmaster@gmail.com>
2019-05-02 14:06:16 +0300

    Fixes some stuff on the dashboards

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -6,7 +6,7 @@
                 {
                     echo '<div class="card mt-2 rounded-0">';
                         echo '<div class="card-header ">';
-                            echo '<h5 class="card-title mb-0">'.$allNews[$i]['title'].'</h5>';
+                            echo '<h5 class="font-weight-bold card-title mb-0">'.$allNews[$i]['title'].'</h5>';
                         echo '</div>';
                         echo '<div class="card-body px-3 py-2">';
                             echo '<div class="card-text" id="'.$i.'" style="overflow:hidden; max-height: 40px;">';
